ea2b1270d3aa6bf7f480df46d7ab343849257b40
KevinT <kevin.tai.mail@gmail.com>
2017-10-27 22:48:43 -0700

    first commit

--- LargestNumber/main.m
+++ LargestNumber/main.m
@@ -10,8 +10,34 @@
 
 int main(int argc, const char * argv[]) {
   @autoreleasepool {
-      // insert code here...
-      NSLog(@"Hello, World!");
+
+//    array with negative number(s) must use double quotes to work
+    NSArray *numbers = @[@"-44", @"0", @"5", @"-100"];
+
+//    highest number in the loop initially set to a number lower than all numbers in the array
+    NSNumber *highestNumber = @-999;
+    
+//    each number of the array gets assigned to NSNumber variable in the loop initially set to 0
+    NSNumber *number = @0;
+    
+//    loop through each number in the array starting from position 0 increment by 1
+    for (int i = 0; i < numbers.count; i++) {
+      
+//      assign each number of the NSArray into NSNumber variable
+      number = numbers[i];
+//      NSLog(@"numbers[%d] = %@, highest number = %@", i, number, highestNumber);
+      
+//      check to see if the highest number found so far is less than current number of the array being evaluated
+      if (highestNumber.intValue < number.intValue) {
+        
+//        assign the current number of the array to be the highest number
+        highestNumber = number;
+      }
+    }
+    
+//    print the highest number of the array
+    NSLog(@"highest number = %@", highestNumber);
+    
   }
   return 0;
 }
